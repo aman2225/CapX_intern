92f8c0c952b11c5ec10dab42960f8133ceb9d531
aman2225 <amankumar74885940@gmail.com>
2025-01-07 00:55:48 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import Navbar from "./components/Navbar";
+// import React from "react";
+import ResponsiveAppBar from "./components/Navbar";
 import Dashboard from "./components/Dashboard/Dashboard";
 const App = () => {
   return (
     <div className="dark:bg-black min-h-screen pt-10">
-      <Navbar />
+      {/* <Navbar /> */}
+      <ResponsiveAppBar/>
       <Dashboard />
     </div>
   );
